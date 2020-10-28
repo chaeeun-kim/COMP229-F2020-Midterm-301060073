7d23cc6cfc3b4f123d986fe00293eae6e04790f3
chaeeun kim <afafpo@naver.com>
2020-10-28 11:20:03 +0900

    message

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://localhost/fav_book"
+  "URI":"mongodb+srv://chaeeun:tUDAqET9izSvedbe@dbserver.kycq9.mongodb.net/fav_book?retryWrites=true&w=majority"
+};
